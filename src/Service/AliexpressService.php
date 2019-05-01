c983bd93046870a82b472418934225e76d469a48
Victor Nazarov <bitlimited.dev@gmail.com>
2019-05-01 14:02:13 +0300

    Aliexpress configuration

--- src/Service/AliexpressService.php
+++ src/Service/AliexpressService.php
@@ -4,6 +4,8 @@
 namespace Bitlimited\Aliexpress\Service;
 
 
+use Bitlimited\Aliexpress\Exception\AliexpressException;
+
 class AliexpressService
 {
     private $email;
@@ -19,14 +21,46 @@ class AliexpressService
         $this->password = $password;
     }
 
-    public function request(array $param)
+    public function request(array $param):array
     {
-        return $this->createAccessToken();
+        $request = [
+            'headers' => [
+                'Authorization' => $this->createAccessToken(),
+                'X-User-Authorization' => $this->createBasicToken()
+            ]
+        ];
+
+        return $request;
     }
 
-    private function createAccessToken()
+    private function createBasicToken():string
     {
-        return $this->email;
+        if(empty($this->password))
+            $this->exceptionInstall("token");
+
+        $token = "Basic ";
+        $token .= base64_encode($this->email . ":" . $this->password);
+
+        return $token;
     }
 
+    private function createAccessToken():string
+    {
+        if(empty($this->email))
+            $this->exceptionInstall("email");
+
+        if(empty($this->token))
+            $this->exceptionInstall("token");
+
+        $token = "AccessToken ";
+        $token .= $this->email . ":";
+        $token .= md5($this->email . gmdate('dmYH') . $this->token);
+
+        return $token;
+    }
+
+    protected function exceptionInstall(string $key)
+    {
+        throw new AliexpressException(sprintf('Empty %s parameter. Install %s for setAuth method',$key, $key),500);
+    }
 }
