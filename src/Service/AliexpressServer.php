7b0275b8ecb17557f319a75c7bda909265566c77
Victor Nazarov <bitlimited.dev@gmail.com>
2019-05-01 08:34:13 +0300

    add run files

---
+++ src/Service/AliexpressServer.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace Bitlimited\Service;
+
+
+class AliexpressServer
+{
+    private $email;
+
+    private $token;
+
+    private $password;
+
+    public function setAuth(string $email, string $token, string $password)
+    {
+        $this->email = $email;
+        $this->token = $token;
+        $this->password = $password;
+    }
+
+    public function request(array $param)
+    {
+        return static::createAccessToken();
+    }
+
+    private function createAccessToken()
+    {
+        return $this->email;
+    }
+
+}
